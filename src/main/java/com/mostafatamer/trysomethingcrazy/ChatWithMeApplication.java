7276917f96d424c539b491c6de3f4ec0cc71b798
Mostafa Tamer <m.tamer.fcai@gmail.com>
2024-07-02 14:30:51 +0300

    editing

--- src/main/java/com/mostafatamer/trysomethingcrazy/ChatWithMeApplication.java
+++ src/main/java/com/mostafatamer/trysomethingcrazy/ChatWithMeApplication.java
@@ -1,21 +1,28 @@
 package com.mostafatamer.trysomethingcrazy;
 
-import com.google.firebase.FirebaseApp;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 
+@Log
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 @RequiredArgsConstructor
 public class ChatWithMeApplication implements CommandLineRunner {
 
-    private final FirebaseApp defaultApp;
+    private final Environment environment;
+
 
     public static void main(String[] args) {
         SpringApplication.run(ChatWithMeApplication.class, args);
@@ -27,7 +34,29 @@ public class ChatWithMeApplication implements CommandLineRunner {
         System.out.println("Running...");
 
         InetAddress ip = InetAddress.getLocalHost();
-        System.out.println("Ip: " + ip.getHostAddress());
+        String port = environment.getProperty("server.port");
 
+        System.out.println("IP: " + ip.getHostAddress());
+        System.out.println("Port: " + port);
+    }
+
+    long numberOfRequests = 0;
+
+    @SneakyThrows
+    @Scheduled(fixedRate = 180000)
+//    @Scheduled(fixedRate = 2000)
+    public void keepServerAwake() {
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = environment.getProperty("server.port");
+
+        String dummyUrl = "http://" + ip + ":" + port + "/auth/hello";
+//        System.out.println(dummyUrl);
+        try {
+            String result = new RestTemplate().getForObject(dummyUrl, String.class);
+            log.info("Request number " + ++numberOfRequests + ": " + result);
+
+        } catch (Exception e) {
+            System.err.println("Failed to send request to dummy route: " + e.getMessage());
+        }
     }
 }
